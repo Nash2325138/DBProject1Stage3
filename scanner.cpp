6a8a6b679c95d1a3f59c622d24db0e20abea8bc1
劉哲宇 <j2325138@gmail.com>
2017-03-04 19:20:19 +0800

    Bug with double quotes

--- scanner.cpp
+++ scanner.cpp
@@ -1,5 +1,7 @@
 #include "scanner.hpp"
 #include <cstdio>
+#include <iostream>
+#include <cstdlib>
 #include <string>
 #include <cstring>
 #include <algorithm>
@@ -11,10 +13,22 @@ Scanner::Scanner(string str): pos(0) {
 Scanner::~Scanner() {}
 	
 string Scanner::nextToken() {
-	char buffer[1000];
+	char buffer[10000];
 	buffer[0] = '\0';
 	int chars_read;
-	if (sscanf(query + pos, " %[A-Za-z_0-9-]%n", buffer, &chars_read)){}
+	if (sscanf(query + pos, " \"%[^\"]\"%n", buffer, &chars_read)) {
+		char temp[10002];
+		strcat(strcat(strcpy(temp, "\""), buffer), "\"");
+		strcpy(buffer, temp);
+		// printf("%s\n", buffer);
+		// exit(EXIT_FAILURE);
+	}
+	else if (sscanf(query + pos, " \'%[^\']\'%n", buffer, &chars_read)) {
+		char temp[10002];
+		strcat(strcat(strcpy(temp, "\'"), buffer), "\'");
+		strcpy(buffer, temp);
+	}
+	else if (sscanf(query + pos, " %[A-Za-z_0-9-]%n", buffer, &chars_read)){}
 	else if (sscanf(query + pos, " %1[^A-Za-z_0-9- \n]%n", buffer, &chars_read)){}
 	else {
 		fprintf(stderr, "sscanf wrong at nextToken()\n");
@@ -23,12 +37,27 @@ string Scanner::nextToken() {
 
 	string ret(buffer);
 	std::transform(ret.begin(), ret.end(), ret.begin(), ::tolower);
+	// if(ret[0] == '\"') {
+	// 	std::cout << ret << '\n';
+	// 	exit(EXIT_FAILURE);
+	// }
 	return ret;
 }
 string Scanner::lookAhead() {
 	char buffer[1000];
 	buffer[0] = '\0';
-	if (sscanf(query + pos, " %[A-Za-z_0-9-]", buffer)){}
+
+	if (sscanf(query + pos, " \"%[^\"]\"%n", buffer)) {
+		char temp[10002];
+		strcat(strcat(strcpy(temp, "\""), buffer), "\"");
+		strcpy(buffer, temp);
+	}
+	else if (sscanf(query + pos, " \'%[^\']\'%n", buffer)) {
+		char temp[10002];
+		strcat(strcat(strcpy(temp, "\'"), buffer), "\'");
+		strcpy(buffer, temp);
+	}
+	else if (sscanf(query + pos, " %[A-Za-z_0-9-]", buffer)){}
 	else if (sscanf(query + pos, " %1[^A-Za-z_0-9- \n]", buffer)){}
 	else {
 		fprintf(stderr, "sscanf wrong at lookAhead()\n");
--- tester/testScanner.cpp
+++ tester/testScanner.cpp
@@ -6,11 +6,14 @@
 using namespace std;
 int main(int argc, char const *argv[])
 {
-	string query_str("The is a string.\nA_B_C,QQ123,(google.com), (-10), kkk-bb");
+	string query_str("The is a string.\nA_B_C,QQ123,(google.com), (-10), kkk-bb, \"LAla- !!~~ k 123\" Aaaaa");//, 'abg e^() wow'");
 	Scanner scanner(query_str);
 	string token;
-	while ((token = scanner.nextToken()) != "") {
+	int i = 0;
+	while ((token = scanner.nextToken()) != "" and i < 50) {
+		// printf("%s\n", token.c_str());
 		cout << token << '\n';
+		i++;
 	}
 	return 0;
 }
