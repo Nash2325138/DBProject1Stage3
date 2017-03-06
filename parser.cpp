3800bf1b1da0f17b3aca8de2cc5828ed98357b35
LiangWei <pinyo9611@gmail.com>
2017-03-06 14:38:03 +0800

    Insert query successfully parsed

--- parser.cpp
+++ parser.cpp
@@ -12,9 +12,11 @@ bool Parser::Parse(){
     string token;
     token = scanner.nextToken();
     if(token == "create") {
-        if (not Create_Table_Query()) return false;
+        isCreateTableQuery = true;
+        if (Create_Table_Query() == false) return false;
     }
     else if(token == "insert") {
+        isInsertQuery = true;
         if (not Insert_Query()) return false;
     }
     else {
@@ -27,7 +29,7 @@ bool Parser::Parse(){
 bool Parser::Create_Table_Query(){
     string token = scanner.nextToken();
     if(token != "table"){
-        fprintf(stderr, "Syntax Error: CREATE TABLE\n");
+        fprintf(stderr, LIGHT_RED "Syntax Error: CREATE TABLE\n" WHITE);
         return false;
     }
     // Table name
@@ -35,7 +37,7 @@ bool Parser::Create_Table_Query(){
     // Check Schema
     token = scanner.nextToken();
     if(token != "("){
-        fprintf(stderr, "Syntax Error: expected '('\n");
+        fprintf(stderr, LIGHT_RED "Syntax Error: expected '('\n" WHITE);
         return false;
     }
     schema.clear();
@@ -49,12 +51,15 @@ bool Parser::Read_Schema() {
     while(true) {
         if (not Read_Attr_Def(attr)) return false;
         schema.push_back(attr);
-        if(scanner.lookAhead() == ")")
-            break;
-        if(scanner.lookAhead() == ",")
+        if(scanner.lookAhead() == ")"){
             scanner.nextToken();
+            break;
+        }
+        else if(scanner.lookAhead() == ","){
+            scanner.nextToken();
+        }
         else{
-            fprintf(stderr, "Syntax Error: expected ','\n");
+            fprintf(stderr, LIGHT_RED "Syntax Error: expected ',' or ')'\n" WHITE);
             return false;
         }
     }
@@ -65,22 +70,22 @@ bool Parser::Read_Attr_Def(Attribute& attr) {
     attr.name = scanner.nextToken();
     attr.type = scanner.nextToken();
     if(attr.type != "int" && attr.type != "varchar"){
-        fprintf(stderr, "Error: unknown type '%s'\n", attr.type.c_str());
+        fprintf(stderr, LIGHT_RED "Error: unknown type '%s'\n" WHITE, attr.type.c_str());
         return false;
     }
     // Read char length
     if(attr.type == "varchar"){
         if(scanner.nextToken() != "("){
-            fprintf(stderr, "Syntax Error: expected '('\n");
+            fprintf(stderr, LIGHT_RED "Syntax Error: expected '('\n" WHITE);
             return false;
         }
         attr.char_len = stoi(scanner.nextToken());
         if(attr.char_len <= 0){
-            fprintf(stderr, "char length should be greater than 0\n");
+            fprintf(stderr, LIGHT_RED "char length should be greater than 0\n" WHITE);
             return false;
         }
         if(scanner.nextToken() != ")"){
-            fprintf(stderr, "Syntax Error: expected ')'\n");
+            fprintf(stderr, LIGHT_RED "Syntax Error: expected ')'\n" WHITE);
             return false;
         }
     }
@@ -88,7 +93,7 @@ bool Parser::Read_Attr_Def(Attribute& attr) {
     if(scanner.lookAhead() == "primary"){
         scanner.nextToken();
         if(scanner.nextToken() != "key"){
-            fprintf(stderr, "Syntax Error: do you mean 'PRIMARY KEY' ?\n");
+            fprintf(stderr, LIGHT_RED "Syntax Error: do you mean 'PRIMARY KEY' ?\n" WHITE);
             return false;
         }
         attr.isPrimaryKey = true;
@@ -103,7 +108,7 @@ bool Parser::Insert_Query() {
     // "into"
     string token = scanner.nextToken();
     if (token != "into") {
-        fprintf(stderr, "Missing 'INTO' after 'CREATE'\n");
+        fprintf(stderr, LIGHT_RED "Missing 'INTO' after 'INSERT'\n" WHITE);
         return false;
     }
 
@@ -112,37 +117,101 @@ bool Parser::Insert_Query() {
 
     if (scanner.lookAhead() == "(") {
         // if there's diretly "(", call Read_Order_Specify()
+        orderSpecified = true;
         if (not Read_Order_Specify()) return false;
-    } else if(scanner.lookAhead() == "values"){
+    } 
+
+    if(scanner.lookAhead() == "values") {
         // call Read_Value()
+        scanner.nextToken();
         if (not Read_Value()) return false;
     } else {
-        fprintf(stderr, "Expected '(' or 'VALUES', got %s\n", scanner.lookAhead());
+        fprintf(stderr, LIGHT_RED "Expected VALUES', got %s\n" WHITE, scanner.lookAhead().c_str());
         return false;
     }
 
     // examine whether order.size() == values.size()
+    if(orderSpecified && order.size() != values.size()){
+        fprintf(stderr, LIGHT_RED "Specified counts must be same as value counts\n" WHITE);
+        return false;
+    }
     return true;
 }
+
 bool Parser::Read_Order_Specify() {
+    string attr_name;
+    order.clear();
     // "("
-    // read attr_name1, attr_name2, attr_name3, ...
-    // and order.push_back(attr_name)
-    // until ")"
+    scanner.nextToken();
+    while(true) {
+        // read attr_name1, attr_name2, attr_name3, ...
+        attr_name = scanner.nextToken();
+        // and order.push_back(attr_name)
+        order.push_back(attr_name);
+        // until ")"
+        if(scanner.lookAhead() == ")"){
+            scanner.nextToken();
+            break;
+        }
+        else if(scanner.lookAhead() == ","){
+            scanner.nextToken();
+        }
+        else{
+            fprintf(stderr, LIGHT_RED "Syntax Error: expected ',' or ')'\n" WHITE);
+            return false;
+        }
+    }
     return true;
 }
+
 bool Parser::Read_Value() {
-    // "values"
-    // read value1, value2, value3, ...
-    // and values.push_back(value)
-    // until ")"
+    string value_name;
+    values.clear();
+    // "(" error detect
+    if(scanner.lookAhead() != "("){
+        fprintf(stderr, LIGHT_RED "Syntax Error: expected '(' after value\n" WHITE);
+        return false;
+    }
+    scanner.nextToken();
+    while(true) {
+        // read value1, value2, value3, ...
+        value_name = scanner.nextToken();
+        // and values.push_back(value)
+        values.push_back(value_name);
+        // until ")"
+        if(scanner.lookAhead() == ")"){
+            scanner.nextToken();
+            break;
+        }
+        else if(scanner.lookAhead() == ","){
+            scanner.nextToken();
+        }
+        else{
+            fprintf(stderr, LIGHT_RED "Syntax Error: expected ',' or ')'\n" WHITE);
+            return false;
+        }
+    }
     return true;
 }
 
 void Parser::Print(){
-    printf("SQL Statement: %s\n", query_str.c_str());
+    printf("SQL Statement:\n%s\n", query_str.c_str());
     printf("Table name: %s\n", table_name.c_str());
-    for(auto &attr:schema) {
-        printf("name: %s, type: %s, isPrimaryKey: %d, char_len: %d\n", attr.name.c_str(), attr.type.c_str(), attr.isPrimaryKey, attr.char_len);
+    if(isCreateTableQuery){
+        for(auto &attr:schema) {
+            printf("name: %s, type: %s, isPrimaryKey: %d, char_len: %d\n", attr.name.c_str(), attr.type.c_str(), attr.isPrimaryKey, attr.char_len);
+        }
+    }
+    else if(isInsertQuery){
+        if(orderSpecified){
+            for(int i=0; i<order.size(); i++){
+                printf("Insert %s: %s into TABLE(%s)\n", order[i].c_str(), values[i].c_str(), table_name.c_str());
+            }
+        }
+        else{
+            for(int i=0; i<values.size(); i++){
+                printf("Insert %s into TABLE(%s)\n", values[i].c_str(), table_name.c_str());
+            }
+        }
     }
 }
--- tester/testParser.cpp
+++ tester/testParser.cpp
@@ -4,9 +4,18 @@
 
 int main(int argc, char const *argv[])
 {
-	string query_str("creAAte tAble aa(name varchar(-10), time intt PRIMARY KEY)");
-	Parser parser(query_str);
-    parser.Parse();
-    parser.Print();
+	string query_str("creAte tAble aa(name varchar(30), time intt PRIMARY KEY)");
+	Parser *parser = new Parser(query_str);
+    if(parser->Parse()){
+    	parser->Print();
+    }
+    delete parser;
+
+    query_str = "INSERT INTO STUDENT (a, b, c, d, e)\nVALUES(1, 1, 3, 'eqweqwe', '123213eqwd')";
+	parser = new Parser(query_str);
+    if(parser->Parse()){
+    	parser->Print();
+    }
+    delete parser;
 	return 0;
 }
