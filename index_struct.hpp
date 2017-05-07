eee8bbf18d830ef7a4159c66ba1bdfdb31f3680f
劉哲宇 <j2325138@gmail.com>
2017-05-07 15:39:42 +0800

    index_struct completed...?

--- index_struct.hpp
+++ index_struct.hpp
@@ -34,8 +34,8 @@ public:
     }
     ~Index_Struct();
     virtual void insert(const Value& v, const int row);
-    virtual const std::vector<int>& fullfill_rows(CompareOP op, const Value& v);
-    virtual const std::vector<int>& fullfill_rows(const Value& v, CompareOP op);
+    virtual const std::vector<int>& satisfied_rows(CompareOP op, const Value& v);
+    virtual const std::vector<int>& satisfied_rows(const Value& v, CompareOP op);
 };
 
 class Hash_Index_Struct: public Index_Struct
@@ -48,7 +48,7 @@ public:
     void insert(const Value& v, const int row) {
         m.insert(std::make_pair(v, row));
     }
-    virtual const std::vector<int>& fullfill_rows(CompareOP op, const Value& v) {
+    const std::vector<int>& satisfied_rows(CompareOP op, const Value& v) {
         static std::vector<int> ret;
         ret.clear();
         // hash map is UNORDERED, so whatever the op is, we need to iterator over all m
@@ -65,9 +65,10 @@ public:
                 if (p.first.smaller(v))
                     ret.push_back(p.second);
         } else if (op == CompareOP::EQUAL) {
-            for (auto& p:m)
-                if (p.first.equal(v))
-                    ret.push_back(p.second);
+            auto iter_pair = m.equal_range(v);
+            for (auto it = iter_pair.first; it != iter_pair.second; it++) {
+                ret.push_back(it->second);
+            }
         } else if (op == CompareOP::NOT_EQUAL) {
             for (auto& p:m)
                 if (!p.first.equal(v))
@@ -76,7 +77,7 @@ public:
         }
         return ret;
     }
-    virtual const std::vector<int>& fullfill_rows(const Value& v, CompareOP op) {
+    const std::vector<int>& fullfill_rows(const Value& v, CompareOP op) {
         static std::vector<int> ret;
         ret.clear();
         // hash map is UNORDERED, so whatever the op is, we need to iterator over all m
@@ -93,9 +94,81 @@ public:
                 if (v.smaller(p.first))
                     ret.push_back(p.second);
         } else if (op == CompareOP::EQUAL) {
+            auto iter_pair = m.equal_range(v);
+            for (auto it = iter_pair.first; it != iter_pair.second; it++) {
+                ret.push_back(it->second);
+            }
+        } else if (op == CompareOP::NOT_EQUAL) {
             for (auto& p:m)
-                if (v.equal(p.first))
+                if (!v.equal(p.first))
                     ret.push_back(p.second);
+        } else {
+        }
+        return ret;
+    }
+};
+
+
+class Tree_Index_Struct: public Index_Struct
+{
+public:
+    std::multimap<Value, int> m;
+    Tree_Index_Struct();
+    Tree_Index_Struct(const std::vector<std::vector<Value>>& tuples, int col):Index_Struct(tuples, col) {};
+    ~Tree_Index_Struct();
+    void insert(const Value& v, const int row) {
+        m.insert(std::make_pair(v, row));
+    }
+    const std::vector<int>& satisfied_rows(CompareOP op, const Value& v) {
+        static std::vector<int> ret;
+        ret.clear();
+        // hash map is UNORDERED, so whatever the op is, we need to iterator over all m
+        if (op == CompareOP::OP_EMPTY) {
+            for (auto& p:m)
+                if (p.first.isTrue())
+                    ret.push_back(p.second);
+        } else if (op == CompareOP::BIGGER) {
+            for (auto& p:m)
+                if (p.first.bigger(v))
+                    ret.push_back(p.second);
+        } else if (op == CompareOP::SMALLER) {
+            for (auto& p:m)
+                if (p.first.smaller(v))
+                    ret.push_back(p.second);
+        } else if (op == CompareOP::EQUAL) {
+            auto iter_pair = m.equal_range(v);
+            for (auto it = iter_pair.first; it != iter_pair.second; it++) {
+                ret.push_back(it->second);
+            }
+        } else if (op == CompareOP::NOT_EQUAL) {
+            for (auto& p:m)
+                if (!p.first.equal(v))
+                    ret.push_back(p.second);
+        } else {
+        }
+        return ret;
+    }
+    const std::vector<int>& fullfill_rows(const Value& v, CompareOP op) {
+        static std::vector<int> ret;
+        ret.clear();
+        // hash map is UNORDERED, so whatever the op is, we need to iterator over all m
+        if (op == CompareOP::OP_EMPTY) {
+            for (auto& p:m)
+                if (v.isTrue())
+                    ret.push_back(p.second);
+        } else if (op == CompareOP::BIGGER) {
+            for (auto& p:m)
+                if (v.bigger(p.first))
+                    ret.push_back(p.second);
+        } else if (op == CompareOP::SMALLER) {
+            for (auto& p:m)
+                if (v.smaller(p.first))
+                    ret.push_back(p.second);
+        } else if (op == CompareOP::EQUAL) {
+            auto iter_pair = m.equal_range(v);
+            for (auto it = iter_pair.first; it != iter_pair.second; it++) {
+                ret.push_back(it->second);
+            }
         } else if (op == CompareOP::NOT_EQUAL) {
             for (auto& p:m)
                 if (!v.equal(p.first))
